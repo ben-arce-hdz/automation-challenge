f73446a516f81538d93e274392fc39c34df39ff9
Benjamin Arce <benjamin_arce@epam.com>
2021-07-31 10:49:24 -0500

    [ADD] - first commit

---
+++ src/main/java/com/wizeline/automationchallenge/AutomationChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wizeline.automationchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wizeline/automationchallenge/AutomationChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizeline.automationchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
